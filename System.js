32396c66589bd200b614f1b032edaf086b49e0d4
01TGMA <Ghostmanhades@gmail.com>
2025-07-05 18:06:07 +0100

    Implemented employee Id  validation and redirects if id exist.
    Fix Bug: implemented a code that checks if an element/id, is in a file. to avoid 'Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')'

--- System.js
+++ System.js
@@ -1,20 +1,41 @@
 const adminBttn = document.getElementById("AdminSubmit");
+const employeeBttn = document.getElementById("Esubmit");
 let admins = [
     {id: 36685, Name: "defualtAdmin"}
 ];
+let employees = [
+    {id: 48684, Name: "defualtAdmin"}
+]
 
-const validateId = () => {
+
+const validateAdminId = () => {
     const adminInput = document.getElementById("AdminId").value.trim();
-    const exists = admins.some( admins => admins.id === Number(adminInput));
-    if (exists){
+    const AdminExists = admins.some( admins => admins.id === Number(adminInput));
+    if(adminInput === "") {
+        console.log("Input ID")
+    }else if (AdminExists){
         console.log("Correct")
         window.location.href = 'Admin_Dashboard.html'
     }else{console.log("wrong ID")} 
 }
+if (adminBttn) {
+    adminBttn.addEventListener("click", validateAdminId);
+  }
 
-adminBttn.addEventListener("click", validateId)
 
-// User Inputs ID, then clicks login button
-// it then tirgers a function that checks if the id exist
-// if it does proceeds to next page, if not displays incorrect id
-// //
+const validateEmployeeId = () => {
+    const employeeInput = document.getElementById("EmployeeId").value.trim();
+    const employeeExist =  employees.some(employees => employees.id === Number(employeeInput));
+    
+    if(employeeInput === "") {
+        console.log("Input ID")
+    }else if (employeeExist) {
+        console.log("Correct")
+        window.location.href = 'Check_In_Out.html'
+    } else{
+        console.log("Wrong ID")
+    }
+}
+if (employeeBttn) {
+    employeeBttn.addEventListener("click", validateEmployeeId)
+  }
